937a36c903f2276d7fc16ec174dda924b9a68989
henryds <henrydwiseptian@gmail.com>
2020-06-02 09:56:27 +0700

    update halaman admin

---
+++ application/views/admin/datasiswa.php
@@ -0,0 +1,42 @@
+
+<div class="container">
+  <div class="col-lg-12 mt-5" style="border: 1px solid #ccc;">
+    <div class="row p-5">
+      <div class="col-lg-12">
+        <h2 class="txt-primary text-center">Data Siswa</h2>
+        <hr class="garisbawah">
+        <div class="row mt-5">
+          <div class="col-lg-12">
+            <table class="table table-striped w-100" border="3" bordercolor="white">
+              <thead>
+                <tr class="text-center">
+                  <th>ID Siswa</th>
+                  <th>Nama Siswa</th>
+                  <th>Tanggal Pendaftaran</th>
+                  <th style="width: 15%">Kelas</th>
+                  <th style="width: 15%">Status</th>
+                  <th style="width: 15%">Rekap Nilai</th>
+                 
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+                 <td><a href="<?= base_url('admin/detailsiswa'); ?>" class="text-danger">Selengkap-nya</a></td>
+                
+                </tr>
+       
+              </tbody>
+
+
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
